33be3ca928bf325f170981f310d4bb8e20ca2e61
hmdyt <konnyaku108@gmail.com>
2021-12-12 22:04:40 +0900

    add  macro mode with output

--- source/Application_Main.cc
+++ source/Application_Main.cc
@@ -1,11 +1,15 @@
 #include "Geometry.hh"
 #include "UserActionInitialization.hh"
+#include "SensitiveDetector.hh"
 
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
+#include "G4SDManager.hh"
+#include "G4VSensitiveDetector.hh"
 #include "FTFP_BERT.hh"
+#include "G4ios.hh"
 
 #include <iostream>
 
@@ -19,6 +23,7 @@ int main(int argc, char** argv){
     G4VisExecutive *visManager = new G4VisExecutive;
     visManager->Initialize();
 
+    G4SDManager* sdManager = G4SDManager::GetSDMpointer();
     G4UImanager* uiManager = G4UImanager::GetUIpointer();
 
     if (argc == 1){
@@ -28,10 +33,21 @@ int main(int argc, char** argv){
         uiManager->ApplyCommand("/control/execute visSetup_Simplex.mac");
         uiExec->SessionStart();
         delete uiExec;
-    } else {
+    } else if (argc == 2) {
         // macro mode
         G4String macroName = argv[1];
-        uiManager->ApplyCommand("/control/execute" + macroName);
+        uiManager->ApplyCommand("/control/execute " + macroName);
+    } else if (argc == 3){
+        // macro mode with output
+        G4String macroName = argv[1];
+        G4String outFileName = argv[2];
+        SensitiveDetector* sensitiveDetector = (SensitiveDetector*)sdManager->FindSensitiveDetector("SensitiveDetector");
+        sensitiveDetector->setOutFileName(outFileName);
+        uiManager->ApplyCommand("/control/execute " + macroName);
+
+    } else {
+        G4cerr << "Invalid argument." << G4endl;
+        return -1;
     }
 
     delete runManager;
