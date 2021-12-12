440f62bd3d3b966b884cb6dc3f53dfc5d8b9e864
hmdyt <konnyaku108@gmail.com>
2021-12-13 01:03:46 +0900

    fix

--- bench/muonBeam1500MeV.sh
+++ bench/muonBeam1500MeV.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# 1.5GeV Muon 10000 * 100 event
+# 1.5GeV Muon 10000 * 1000 event
 # cut neutral particle
 # only save in sensitive detector
 # execute this file at /mcMuonPhotoProduction/bench/.
 
-for i in {0..99}
+for i in {0..999}
 do
     ../bin/Application_Main muonBeam1500MeV.mac "/data/hamada/geant4_data/mcMuonPhotoProduction/muomBeam1500MeV_${i}.root"
 done
