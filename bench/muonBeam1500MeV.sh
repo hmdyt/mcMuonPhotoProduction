7ff531ceabbac32ae47b66719e2ee5f149edd647
hmdyt <konnyaku108@gmail.com>
2021-12-13 01:06:11 +0900

    fix typo

--- bench/muonBeam1500MeV.sh
+++ bench/muonBeam1500MeV.sh
@@ -6,5 +6,5 @@
 
 for i in {0..999}
 do
-    ../bin/Application_Main muonBeam1500MeV.mac "/data/hamada/geant4_data/mcMuonPhotoProduction/muomBean1500MeV_${i}.root"
+    ../bin/Application_Main muonBeam1500MeV.mac "/data/hamada/geant4_data/mcMuonPhotoProduction/muonBeam1500MeV_${i}.root"
 done
