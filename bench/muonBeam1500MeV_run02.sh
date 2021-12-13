5241f026f5eb0b6bca87f1d1e51b0eac21462aaf
hmdyt <konnyaku108@gmail.com>
2021-12-13 23:16:00 +0900

    add progress output

--- bench/muonBeam1500MeV_run02.sh
+++ bench/muonBeam1500MeV_run02.sh
@@ -3,8 +3,14 @@
 # cut neutral particle
 # only save in sensitive detector
 # execute this file at /mcMuonPhotoProduction/bench/.
-
+n=$(sqrt 1000 - 5999)
 for i in {1000..5999}
 do
     ../bin/Application_Main muonBeam1500MeV.mac "/data/hamada/geant4_data/mcMuonPhotoProduction/muonBeam1500MeV_${i}.root"
+
+    # progress output
+    if [ 0 -eq $(expr $i % $(expr $n / 100)) ]
+    then
+        echo $(expr $(expr 100 \* $i) / $n)/100%
+    fi
 done
