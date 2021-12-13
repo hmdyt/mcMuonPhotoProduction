a2994f9c9004e2ec4364dfd39bef9eaa599c4620
hmdyt <konnyaku108@gmail.com>
2021-12-13 15:33:43 +0900

    add checing

--- analyze/src/makePionTrackHist.cc
+++ analyze/src/makePionTrackHist.cc
@@ -6,16 +6,29 @@
 #include <string>
 #include <fstream>
 
-std::vector<int> makePionEventList(TChain* chain){
+std::vector<int> makePionEventList(TChain* chain, bool use_chache = true){
     std::cout << "pion event search start ..." << std::endl;
     if (chain == nullptr) {return {-1};}
 
+    // chache return
+    ifstream ifs("makePionEventList.chache");
+    int buff;
+    std::vector<int> retChache = {};
+    if (use_chache && ifs){
+    while (ifs >> buff){
+        retChache.push_back(buff);
+    }
+    return retChache;
+    }
+    
+    // setbranch address
     chain->SetBranchStatus("*", 0);
     chain->SetBranchStatus("particleName", 1);
     std::vector<std::string>* particleName = 0;
     chain->SetBranchAddress("particleName", &particleName);
     int nEntries = chain->GetEntries();
 
+    // search pion event
     std::vector<int>  pionEventList = {};
     bool isExistPion;
     for (int i = 0; i < nEntries; i++){
@@ -38,7 +51,11 @@ std::vector<int> makePionEventList(TChain* chain){
     }
 
     // make chache
-
+    std::ofstream ofs("makePionEventList.chache", std::ios::trunc);
+    for (int i = 0; i < pionEventList.size(); i++){
+        ofs << pionEventList.at(i) << " ";
+    }
+    std::cout << "chache saved" << std::endl;
 
     return pionEventList;
 }
