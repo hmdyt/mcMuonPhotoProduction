0e43061f0f12bada3ead1082dcb178f413671d1d
tomoe <t.peanuts0115@gmail.com>
2021-12-28 16:15:07 +0900

    change world placement

--- source/src/Geometry.cc
+++ source/src/Geometry.cc
@@ -62,8 +62,8 @@ G4LogicalVolume* Geometry::constructLogAlminumBox()
          solid_Albox,
          solid_Albox_hole,
          new G4RotationMatrix(),
-         G4ThreeVector(0, 0, 4 * i * cm)
-      );
+         G4ThreeVector( 0, 0, 4 * i * cm)
+         );
    }
    
    G4Material* materi_AlBox = materi_Man->FindOrBuildMaterial("G4_Al");
@@ -160,8 +160,11 @@ G4VPhysicalVolume* Geometry::Construct()
 
    // placement world
    G4int copyNum_World = 0;
+   G4ThreeVector world_placement( 1 * m, 0, 0);
+   G4RotationMatrix world_rotation;
+   world_rotation = G4RotationMatrix();
    G4PVPlacement* physVol_World  = new G4PVPlacement(
-      G4Transform3D(),
+      G4Transform3D(world_rotation, world_placement),
       "PhysVol_World",
       logVol_World,
       0,
